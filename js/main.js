4f302e081b708e4fa80e80378d1a884dea744edf
katherine <kretamales280@gmail.com>
2019-03-24 22:31:44 -0300

    Estructurando el portafolio

---
+++ js/main.js
@@ -0,0 +1,6 @@
+
+document.getElementById('btn1').addEventListener('click',(e)=>{
+    e.preventDefault();
+    document.getElementById('menu').style.display ='none';
+    document.getElementById('myProfile').style.display = 'block';
+});
