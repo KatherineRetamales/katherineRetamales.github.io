13d1db347e10d15b98af88f617c58418418a69e7
katherine <kretamales280@gmail.com>
2019-03-25 01:14:53 -0300

    Estan funcionando los botones

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,88 @@
 document.getElementById('btn1').addEventListener('click',(e)=>{
     e.preventDefault();
     document.getElementById('menu').style.display ='none';
+    document.getElementById('skills').style.display ='none';
+    document.getElementById('projects').style.display ='none';
+    document.getElementById('contact').style.display ='none';
     document.getElementById('myProfile').style.display = 'block';
+    document.getElementById('btnMenu').style.display = 'block';
+});
+
+document.getElementById('btn2').addEventListener('click',(e)=>{
+    e.preventDefault();
+    document.getElementById('menu').style.display ='none';
+    document.getElementById('skills').style.display ='block';
+    document.getElementById('projects').style.display ='none';
+    document.getElementById('contact').style.display ='none';
+    document.getElementById('myProfile').style.display = 'none';
+    document.getElementById('btnMenu').style.display = 'block';
+});
+
+document.getElementById('btn3').addEventListener('click',(e)=>{
+    e.preventDefault();
+    document.getElementById('menu').style.display ='none';
+    document.getElementById('skills').style.display ='none';
+    document.getElementById('projects').style.display ='block';
+    document.getElementById('contact').style.display ='none';
+    document.getElementById('myProfile').style.display = 'none';
+    document.getElementById('btnMenu').style.display = 'block';
+});
+
+document.getElementById('btn4').addEventListener('click',(e)=>{
+    e.preventDefault();
+    document.getElementById('menu').style.display ='none';
+    document.getElementById('skills').style.display ='none';
+    document.getElementById('projects').style.display ='none';
+    document.getElementById('contact').style.display ='block';
+    document.getElementById('myProfile').style.display = 'none';
+    document.getElementById('btnMenu').style.display = 'block';
+});
+
+document.getElementById('btn11').addEventListener('click',(e)=>{
+    e.preventDefault();
+    document.getElementById('menu').style.display ='none';
+    document.getElementById('skills').style.display ='none';
+    document.getElementById('projects').style.display ='none';
+    document.getElementById('contact').style.display ='none';
+    document.getElementById('myProfile').style.display = 'block';
+    document.getElementById('btnMenu').style.display = 'block';
+});
+
+document.getElementById('btn22').addEventListener('click',(e)=>{
+    e.preventDefault();
+    document.getElementById('menu').style.display ='none';
+    document.getElementById('skills').style.display ='block';
+    document.getElementById('projects').style.display ='none';
+    document.getElementById('contact').style.display ='none';
+    document.getElementById('myProfile').style.display = 'none';
+    document.getElementById('btnMenu').style.display = 'block';
+});
+
+document.getElementById('btn33').addEventListener('click',(e)=>{
+    e.preventDefault();
+    document.getElementById('menu').style.display ='none';
+    document.getElementById('skills').style.display ='none';
+    document.getElementById('projects').style.display ='block';
+    document.getElementById('contact').style.display ='none';
+    document.getElementById('myProfile').style.display = 'none';
+    document.getElementById('btnMenu').style.display = 'block';
+});
+
+document.getElementById('btn44').addEventListener('click',(e)=>{
+    e.preventDefault();
+    document.getElementById('menu').style.display ='none';
+    document.getElementById('skills').style.display ='none';
+    document.getElementById('projects').style.display ='none';
+    document.getElementById('contact').style.display ='block';
+    document.getElementById('myProfile').style.display = 'none';
+    document.getElementById('btnMenu').style.display = 'block';
+});
+document.getElementById('btn55').addEventListener('click',(e)=>{
+    e.preventDefault();
+    document.getElementById('menu').style.display ='block';
+    document.getElementById('skills').style.display ='none';
+    document.getElementById('projects').style.display ='none';
+    document.getElementById('contact').style.display ='none';
+    document.getElementById('myProfile').style.display = 'none';
+    document.getElementById('btnMenu').style.display = 'none';
 });
