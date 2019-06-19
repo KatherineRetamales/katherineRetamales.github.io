5bd1bbe467dc317334edac73ca41684fe2f31edd
KatherineRetamales <kretamales280@gmail.com>
2019-06-19 01:01:14 -0400

    se actualizo js y css

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
-function myFunction() 
-{
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-}
+
+      var myVar;
+
+      function myFunction() {
+        myVar = setTimeout(showPage, 3000);
+      }
+
+      function showPage() {
+        document.getElementById("loader").style.display = "none";
+        document.getElementById("myDiv").style.display = "block";
+      }
